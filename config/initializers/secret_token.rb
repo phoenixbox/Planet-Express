8befb0c2e37cce0b7d664a7ffd4d7cfe433a588a
Shane Rogers <rogerssh@tcd.ie>
2013-12-14 17:09:01 -0700

    app initialization with gemfile updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlanetExpress::Application.config.secret_key_base = '7261520d0d06085a938813b66fdf9077430c4b8f1fa4d917e6230c7802816f798e461db50a1c597ab991837e530240bb252a9f74ecfc8e6596518a8e4f26f8d0'
